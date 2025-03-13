03ab363e308a8f921b9687b15bb07d1cb5804121
Scott Hebert <scoheb@gmail.com>
2025-03-12 22:05:21 -0400

    fix: revert remove install crd
    Signed-off-by: Scott Hebert <scoheb@gmail.com>

--- tasks/managed/collect-data/tests/pre-apply-task-hook.sh
+++ tasks/managed/collect-data/tests/pre-apply-task-hook.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Install the CRDs so we can create/get them
+.github/scripts/install_crds.sh
+
 # Add RBAC so that the SA executing the tests can retrieve CRs
 kubectl apply -f .github/resources/crd_rbac.yaml
 
