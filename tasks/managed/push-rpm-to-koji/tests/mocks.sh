fca72491d7ec44799a186c3ecb331695c163826a
Scott Hebert <scoheb@gmail.com>
2025-06-25 16:28:06 -0400

    feat(RELEASE-1718): ta in push-rpm-to-koji
    - enable trusted artifacts in push-rpm-to-koji task
    
    Signed-off-by: Scott Hebert <scoheb@gmail.com>

--- tasks/managed/push-rpm-to-koji/tests/mocks.sh
+++ tasks/managed/push-rpm-to-koji/tests/mocks.sh
@@ -3,8 +3,8 @@ set -eux
 
 # mocks to be injected into task step scripts
 function kinit() {
-    echo "kinit $@" >> "$(workspaces.data.path)/kinit_calls.txt"
-    call_count=$(wc -l < "$(workspaces.data.path)/kinit_calls.txt")
+    echo "kinit $@" >> "$(params.dataDir)/kinit_calls.txt"
+    call_count=$(wc -l < "$(params.dataDir)/kinit_calls.txt")
     # Simulate kinit failures on first two calls
     if [[ "$call_count" < 3 ]]; then
         echo "kinit failed"
@@ -13,7 +13,7 @@ function kinit() {
 }
 
 function oras() {
-    echo "oras $@" >> "$(workspaces.data.path)/oras_calls.txt"
+    echo "oras $@" >> "$(params.dataDir)/oras_calls.txt"
     if [[ "$1" == "manifest" && "$2" == "fetch" ]]; then
         echo '{"annotations": {"koji.build-target": "mock-target"}}'
     elif [[ "$1" == "pull" ]]; then
@@ -23,7 +23,7 @@ function oras() {
 }
 
 function koji() {
-    echo "koji $@" >> "$(workspaces.data.path)/koji_calls.txt"
+    echo "koji $@" >> "$(params.dataDir)/koji_calls.txt"
     if [[ "$*" == *CGInitBuild* ]]; then
         echo '{"build_id": 12345, "token": "mock-token"}'
     fi
