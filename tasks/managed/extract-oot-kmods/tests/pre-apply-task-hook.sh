539f53be60286b5ca564d845d1e07e2d2af201ff
Scott Hebert <scoheb@gmail.com>
2025-08-12 22:08:26 -0400

    feat(RELEASE-1816): remove workspaces from tasks
    - remove workspace and workspace bindings from extract-index-image,
      extract-oot-kmods and filter-already-released-advisory-images
    - update workspace usage for above tasks in pipelines in managed/
    - remove skip and patch trusted artfacts steps in above tasks
    
    Signed-off-by: Scott Hebert <scoheb@gmail.com>

--- tasks/managed/extract-oot-kmods/tests/pre-apply-task-hook.sh
+++ tasks/managed/extract-oot-kmods/tests/pre-apply-task-hook.sh
@@ -4,7 +4,7 @@
 #SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 # Add mocks to the beginning of task step script
-#yq -i '.spec.steps[0].script = load_str("'$SCRIPT_DIR'/mocks.sh") + .spec.steps[0].script' "$TASK_PATH"
+#yq -i '.spec.steps[1].script = load_str("'$SCRIPT_DIR'/mocks.sh") + .spec.steps[1].script' "$TASK_PATH"
 
 
 
@@ -15,4 +15,4 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 # Add mocks to the beginning of the extract-kmods step script
 # The extract-kmods step is step[2] in the trusted artifacts structure
-yq -i '.spec.steps[2].script = load_str("'$SCRIPT_DIR'/mocks.sh") + .spec.steps[2].script' "$TASK_PATH"
+yq -i '.spec.steps[1].script = load_str("'$SCRIPT_DIR'/mocks.sh") + .spec.steps[1].script' "$TASK_PATH"
